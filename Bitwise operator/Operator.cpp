021dd05fce588aeb8ef96f2f8ed92af2346613d4
Suman Maity <someone.cpp@proton.me>
2025-03-27 21:04:37 +0530

    Add files via upload

--- Bitwise operator/Operator.cpp
+++ Bitwise operator/Operator.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
+int main (){
+   int a = 3, b = 7;
+    //    cout<<(a & b);
+    //    cout<<(a | b);
+    //   cout<<(a ^ b);
+    // cout<<(10 << 2);
+    cout<<(10 >> 1);
 
+    // bitwise << ans:- a * 2^b
+    // bitwise >> ans:- a / 2^b
+
+    /*
+    Operator Priority
+    !, +, -       R to L
+    *, /, %
+    +, -
+    <, <=, >=, >
+    ==, !=
+    &&
+    ||
+    =
+    
+    */
+    return 0;
+}
---
+++ Bitwise operator/Scope.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main (){
+// We can't acess any variables outside the scope
+//    {
+//     int x = 10;
+//    }
+
+   //cout<<x;
+
+//    int x ;
+//    float y;
+//    char z;
+//    string name;
+//    double a;
+//    long b;
+//    long long c;
+//    signed d;
+//    unsigned e;
+//    short f;
+
+//    cout<<sizeof(x)<<endl;
+//    cout<<sizeof(y)<<endl;
+//    cout<<sizeof(z)<<endl;
+//    cout<<sizeof(name)<<endl;
+//    cout<<sizeof(a)<<endl;
+//    cout<<sizeof(b)<<endl;
+//    cout<<sizeof(c)<<endl;
+//    cout<<sizeof(d)<<endl;
+//    cout<<sizeof(e)<<endl;
+//    cout<<sizeof(f)<<endl;
+
+    //  cout<<sizeof(int)<<endl;
+    //  cout<<sizeof(long int)<<endl;
+    //  cout<<sizeof(short int)<<endl;
+
+    unsigned int x = -10;
+    cout<<x;
+
+    return 0;
+}
