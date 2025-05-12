8418ff9d808afdba868f711ea026d8ecf2bc5384
Suman Maity <someone.cpp@proton.me>
2025-05-13 01:16:25 +0530

    Add files via upload

---
+++ Pointer/basic.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+// int a;
+// cout<< &a <<endl;
+
+int a;
+int* ptr = &a;
+
+cout<< &a <<endl;
+cout<< ptr <<endl;
+cout<< &ptr <<endl;
+
+
+    return 0;
+}
