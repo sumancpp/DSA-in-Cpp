e13848057507797d4f92469bc21ccb8827ccd28c
Suman Maity <someone.cpp@proton.me>
2025-05-14 22:12:14 +0530

    Add files via upload

---
+++ Pointer/null_pointer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+int *ptr = NULL;
+ 
+
+   // practice question
+   int a = 5;
+   int *p = &a;
+   int **q = &p;
+
+   cout<< *p <<endl;//5
+   cout<< **q <<endl;//5
+   cout<< p <<endl;
+   cout<< *q <<endl;
+
+    return 0;
+}
