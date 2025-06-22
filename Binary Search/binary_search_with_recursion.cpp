dd6dfcf6d9895d416f19b9f60980268d6d548147
Suman Maity <someone.cpp@proton.me>
2025-06-23 00:44:28 +0530

    Add files via upload

---
+++ Binary Search/binary_search_with_recursion.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int recbinsearch(vector<int>arr, int target, int start, int end){
+    if(start <= end){
+      int mid = start + (end - start) / 2;
+      if(target > arr[mid]){ //2nd half
+        return recbinsearch(arr, target, mid+1, end);
+      }
+      else if(target < arr[mid]){ //1st half
+        return recbinsearch(arr, target, start, mid-1);
+      }
+      else{
+        return mid;
+      }
+    }
+    return -1;
+}
+int main(){
+  
+vector<int>arr={1, 2, 7, 9, 12};
+int size = 5;
+int target;
+cout<<"Enter your target ";
+cin>>target;
+cout<<"You found your target at the index of "<<recbinsearch(arr, target, 0, size-1);
+
+    return 0;
+}
