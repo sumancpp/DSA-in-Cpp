4b0d3d7ea66795f1000e7725cc02c2f4e5a5e0fa
Suman Maity <someone.cpp@proton.me>
2025-12-03 01:42:24 +0530

    Update buy_and_sell.cpp

--- Array/Buy and Sell stock problem/buy_and_sell.cpp
+++ Array/Buy and Sell stock problem/buy_and_sell.cpp
@@ -5,12 +5,15 @@ int main(){
     int size = 6;
     int best_buy = price[0];
     int max_profit = 0;
-    for(int i = 0; i < size; i++){
-        if(price[i] > best_buy){
+    for(int i = 0; i < size; i++)
+    {
+        if(price[i] > best_buy)
+        {
          max_profit = max(max_profit,price[i] - best_buy);
         }
       best_buy = min(best_buy, price[i]);
     }
      cout<<"The best buy is "<<max_profit;
     return 0;
+
 }
