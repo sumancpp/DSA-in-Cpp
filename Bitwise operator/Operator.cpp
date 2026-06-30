8fa12ebb858b0451dfd76f0aa8b70270ce42ff9d
Suman Maity <someone.cpp@proton.me>
2026-07-01 00:46:10 +0530

    Fix formatting and restore main function in Operator.cpp

--- Bitwise operator/Operator.cpp
+++ Bitwise operator/Operator.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
-int main (){
+int main ()
+{
    int a = 3, b = 7;
     //    cout<<(a & b);
     //    cout<<(a | b);
-    //   cout<<(a ^ b);
-    // cout<<(10 << 2);
-    cout<<(10 >> 1);
+    //    cout<<(a ^ b);
+    //    cout<<(10 << 2);
+          cout<<(10 >> 1);
 
     // bitwise << ans:- a * 2^b
     // bitwise >> ans:- a / 2^b
