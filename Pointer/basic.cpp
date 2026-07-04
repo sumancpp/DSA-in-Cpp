a0fe5da6ddc79f31ac9a7b5ebf6d4c5ee3bef2ea
Suman Maity <someone.cpp@proton.me>
2026-07-04 21:25:49 +0530

    Enhance comments and formatting in basic.cpp
    Updated comments to clarify pointer usage and added formatting.

--- Pointer/basic.cpp
+++ Pointer/basic.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 using namespace std;
-int main(){
 
-// int a;
-// cout<< &a <<endl;
+int main() {
 
-int a;
-int* ptr = &a;
+    // int a;
+    // cout<< &a <<endl;
 
-cout<< &a <<endl;
-cout<< ptr <<endl;
-cout<< &ptr <<endl;
+    int a;
 
+    // Declare a pointer variable named 'ptr' and initialize it with the memory address of 'a'
+    // The '*' signifies that 'ptr' is a pointer, and '&' is the address-of operator.
+    int* ptr = &a;
+
+    // Print the memory address where the variable 'a' is stored
+    cout << &a << endl;
+
+    // Print the value stored in 'ptr', which is also the memory address of 'a'
+    cout << ptr << endl;
+
+    // Print the memory address of the pointer variable 'ptr' itself
+    cout << &ptr << endl;
 
     return 0;
 }
