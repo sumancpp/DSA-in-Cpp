d0acd2b9c36fe75b65558c6482a8bd75209e6090
Suman Maity <someone.cpp@proton.me>
2026-07-23 12:34:53 +0530

    Add files via upload

---
+++ Variable, Data Types & Operators/variable.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+//A variable is a named memory location used to store data.
+
+// Syntax :- dataType variableName = value;
+
+int marks = 90;
+float salary = 35000.50;
+char grade = 'A';
+bool passed = true;
+
+cout<<marks<<"\n"<<salary<<"\n"<<grade<<"\n"<<passed;
+
+
+// Variable Naming Rules
+
+// ✅ Allowed
+
+// student
+// student1
+// student_name
+// _age
+
+// ❌ Not Allowed
+
+// 1student      // starts with number
+// student-name  // contains -
+// int           // keyword
+
+
+
+
+
+
+// Declaring vs Initializing
+// Declaration
+// int age;
+
+// Memory is created.
+
+// Initialization
+// int age = 20;
+
+// Memory + value assigned.
+
+
+int age = 20;
+
+age = 25;
+
+cout << age;
+
+
+
+
+
+    return 0;
+}
