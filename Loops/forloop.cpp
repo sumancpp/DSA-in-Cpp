90a20962cb02471489bccaf942ef18661b56a1fc
Suman Maity <someone.cpp@proton.me>
2026-08-06 21:35:48 +0530

    Add files via upload

---
+++ Loops/forloop.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+// for(initialization; condition; update)
+// {
+//     // body
+// }
+
+
+// for(int i=1; i<=5; i++){
+//     cout<< i <<" ";
+// }
+
+// cout<<endl;
+
+// Reverse Loop
+// for(int i=10;i>=1;i--)
+// {
+//     cout<<i<<" ";
+// }
+
+// Infinite for Loop
+// for(;;)
+// {
+//     cout<<"Hello";
+// }
+
+// Multiple Variables
+// for(int i=0,j=10;i<=5;i++,j--)
+// {
+//     cout<<i<<" "<<j<<endl;
+// }
+
+
+
+// Omitting Parts
+// int i=1;
+// for(;i<=5;i++)
+// {
+//     cout<<i;
+// }
+
+// int i=1;
+
+// for(;i<=5;)
+// {
+//     cout<<i;
+//     i++;
+// }
+
+
+
+
+
+
+int i;
+
+for(i=0;i<5;i++)
+{
+
+}
+
+cout<<i;
+
+
+    return 0;
+}
