9d67ecc0b65fa6faada818cff5473155ccd017d3
Suman Maity <someone.cpp@proton.me>
2026-08-13 01:06:23 +0530

    Add other pattern question problems

---
+++ Pattern Question/problem05.cpp
@@ -0,0 +1,19 @@
+// print triangle pattern 
+// *
+// * *
+// * * *
+// * * * *
+
+#include<iostream>
+using namespace std;
+int main(){
+
+int n=4;
+for(int i=1; i<=n; i++){
+    for(int j=1; j<=i; j++){
+        cout<<"*"<<" ";
+    }
+    cout<<endl;
+}
+    return 0;
+}
---
+++ Pattern Question/problem07.cpp
@@ -0,0 +1,20 @@
+// print A 
+//       B B 
+//       C C C 
+//       D D D D
+
+#include<iostream>
+using namespace std;
+int main(){
+
+int n = 4;
+char ch = 'A';
+for(int i=1; i<=n; i++){
+    for(int j=1; j<=i; j++){
+        cout<<ch<<" ";
+    }
+    ch = ch + 1;
+    cout<<endl;
+}
+    return 0;
+}
