ffadb63b0e129964b1ff5a3afbc3c204284c43b4
Suman Maity <someone.cpp@proton.me>
2026-08-30 23:46:04 +0530

    Update function.cpp

--- Function/function.cpp
+++ Function/function.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 using namespace std;
-
 // function defination
   int printHello (){
     cout<<"Hello\n";
     return 5;
   }
 int main(){
-
     //function call / invoke
     // int val = printHello ();
     // cout<<"Val is = "<<val;
