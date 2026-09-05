72349c746bebcc912f8b80e50b4fdda794f3e6f6
Suman Maity <someone.cpp@proton.me>
2026-09-05 23:35:57 +0530

    Update swap_max_min_number.cpp

--- Array/Basic Array/swap_max_min_number.cpp
+++ Array/Basic Array/swap_max_min_number.cpp
@@ -19,20 +19,16 @@ void swap(int arr[], int size) {
 int main(){
  int arr[] = {1, 2, 3, 4, 5, 6};
     int size = sizeof(arr) / sizeof(arr[0]);
-
     cout << "The original array is: ";
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
-
     swap(arr, size);
-
     cout << "After swapping max and min: ";
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
-            
       return 0;
     }
